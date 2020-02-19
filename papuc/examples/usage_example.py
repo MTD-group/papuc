d2a6e954d6aaee53a883076a001a9387ee657d22
Michael Waters <michael.j.waters@northwestern.edu>
2020-02-19 13:55:04 -0600

    I added images for the documentation.

--- papuc/examples/usage_example.py
+++ papuc/examples/usage_example.py
@@ -1,35 +1,36 @@
-
 from matplotlib import pyplot as plt
 import numpy as np
-
-
-######### this first part synthesizes some data to try
+##### Synthesizes some data to plot
 npts = 16
 xmax = 2
 ymax = 1
-X, Y = np.meshgrid(np.linspace(0, xmax , xmax * npts), np.linspace(0, ymax , ymax*npts))
+X, Y = np.meshgrid(
+    np.linspace(0, xmax , xmax * npts),
+    np.linspace(0, ymax , ymax * npts))
 U = np.sin(2*np.pi*Y)
 V = np.cos(2*np.pi*X)
 angle = np.arctan2(V,U)
 magnitude = np.sqrt(V**2 + U**2)
 magnitude_norm = magnitude/magnitude.max()
 
-#####################
+###### Now for visualization 
 from papuc.example_maps import colormaps
-my_map =colormaps['default']
+my_map = colormaps['default']
 
+## make an sRGB1 image from the data
 image = my_map(angle, magnitude_norm)
-### that's all you need!
 
-# this is what it looks like
-plt.imshow(image, origin = 'lower',  extent = (0, xmax, 0, ymax) , interpolation ='None' )
+## this is what it looks like with a quiver plot for comparison
+plt.imshow(image, origin = 'lower',  extent = (0, xmax, 0, ymax) )
 plt.quiver(X, Y, U, V, units='width')
 
-# save the image
+## save the image
 plt.imsave('test_image.png', image, origin = 'lower')
 
+## save the combined figure
 plt.savefig('test_figure.png')
-### this is for looking at your colormap's color wheel
+
+## this is for looking at your colormap's color wheel
 fig, ax = plt.subplots()
 from papuc.analysis import plot_colorwheel
 plot_colorwheel(ax, my_map)

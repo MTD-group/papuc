502f1505cc498f37f27831b707a06c7bdb2daefa
Michael Waters <mjwaters@umich.edu>
2020-02-24 15:26:20 -0600

    The polygon lines were showing up so I set an edge color to fix it.

--- papuc/__init__.py
+++ papuc/__init__.py
@@ -1,3 +1,16 @@
 from .core import isoluminant_uniform_spline_colormap,  isoluminant_uniform_circle_colormap
 from .analysis import plot_knots_on_isoluminant_slice, plot_colorwheel
 from .example_maps import colormaps
+
+
+
+from .core import isoluminant_uniform_spline_colormap, periodic_spline, periodic_spline_test, default_colorspace
+from .analysis import plot_knots_on_isoluminant_slice, plot_colorwheel, cyclic_colormap_test
+
+
+# these functions are exposed to the user for development purposes, you don't need them for using a prexisting color map
+from .analysis import (plot_analytic_vortex_test_data,
+	plot_small_wave_magnitude_test,
+	plot_small_wave_angle_test,
+	plot_perceptual_derivative,
+	plot_arc_length_vs_spline_parameter_t)
--- papuc/plot_3d.py
+++ papuc/plot_3d.py
@@ -1,4 +1,4 @@
-from mpl_toolkits.mplot3d import Axes3D 
+from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,14 +21,14 @@ def color_triangle_mesh(
                 num_z_levels = 16,
                 magnitude = 1.0,
                 verbose = False):
-    
-    
+
+
     def eval_to_lab(theta, mag):
             a = my_map.a_func_of_theta(theta)
             b = my_map.b_func_of_theta(theta)
             lab_color = mag*np.array([my_map.L_max,a,b])
             return lab_color
-    
+
     def eval_vert_to_lab(vert):
         out = np.zeros((len(vert),3))
         for i in range(len(vert)):
@@ -39,7 +39,7 @@ def color_triangle_mesh(
             out[i] = mag*np.array([my_map.L_max,a,b])
         return out
 
-    
+
     mode = '2D'
     mode.upper()
 
@@ -48,7 +48,7 @@ def color_triangle_mesh(
         height = magnitude
         delta = width/num_theta_levels
         deltay = -height/num_z_levels
-    
+
 
 
     vert_list = []
@@ -57,7 +57,7 @@ def color_triangle_mesh(
     if True:
         for iy in range(num_z_levels):
             for ix in range(num_theta_levels):
-                # low 
+                # low
                 theta = delta*(1/3.0+ix)
                 mag   = height + deltay*(iy+1/3.0)
                 color_list.append(eval_to_lab(theta,mag))
@@ -66,7 +66,7 @@ def color_triangle_mesh(
                             (delta*(1.0+ix),  height + deltay*(iy)),
                             (delta*(ix    ),  height + deltay*(iy+1.0))]
                 vert_list.append(eval_vert_to_lab(vertices))
-                
+
                 # high
                 theta = delta*(2/3.0+ix)
                 mag   = height + deltay*(iy+2/3.0)
@@ -76,7 +76,7 @@ def color_triangle_mesh(
                             (delta*(1.0+ix),  height + deltay*(iy+1.0)),
                             (delta*(ix    ),  height + deltay*(iy+1.0))]
                 vert_list.append(eval_vert_to_lab(vertices))
-    
+
     else: # this was a strange idea below
         if verbose: print('downwards triangles')
         for iy in range(nsegs):
@@ -85,12 +85,12 @@ def color_triangle_mesh(
                 #f3 = (iy+0.5)*(1.0/nsegs)
                 #fracs = np.array([1-f2-f3,f2,f3])
                 #fracs = (1/nsegs) * np.array([nsegs-ix-1/3.-iy-1/3., ix+1/3., iy+1/3.])
-                #color = fracs.dot(cp) 
+                #color = fracs.dot(cp)
 
                 theta = delta*(0.5+0.5*iy+ix)
                 mag   = height + deltay*(iy+1/3.)
-                
-                
+
+
 
                 if mode.upper() =='2D':
                     vertices = [
@@ -98,41 +98,41 @@ def color_triangle_mesh(
                             (delta*(0.5+0.5*iy+ix),  height + deltay*(iy+1)),
                             (delta*(1.0+0.5*iy+ix),  height + deltay*   iy)]
                 else:
-                    fracs = (1/nsegs) * np.array([  
+                    fracs = (1/nsegs) * np.array([
                                         [nsegs-ix-iy,   ix,   iy  ],
                                         [nsegs-ix-1-iy, ix+1, iy  ],
                                         [nsegs-ix-iy-1, ix  , iy+1]])
                     vertices = fracs.dot(cp)
                 vert_list.append(eval_vert_to_lab(vertices))
                 color_list.append(eval_to_lab(theta,mag))
-                
+
         if verbose: print('upwards triangles')
         for iy in range(nsegs-1):
             for ix in range(nsegs-iy-1):
                 #the centroids of 45-45-90 right triangles in this space
                 #fracs = (1/nsegs) * np.array([nsegs-ix-2./3-iy-2./3, ix+2./3, iy+2./3])
-                #color = fracs.dot(cp) 
-                
+                #color = fracs.dot(cp)
+
                 theta = delta*(1.0+0.5*iy+ix)
                 mag   = height + deltay*(iy+2/3.)
-                
+
                 if mode.upper()=='2D':
                     vertices = [
                                 (delta*(0.5+0.5*iy+ix), height + deltay *(iy+1)),
                                 (delta*(1.0+0.5*iy+ix), height + deltay * iy   ),
                                 (delta*(1.5+0.5*iy+ix), height + deltay *(iy+1))]
                 else:
-                    fracs = (1/nsegs) * np.array([  
+                    fracs = (1/nsegs) * np.array([
                                         [nsegs-ix-1.0-iy,     ix+1.0,  iy    ],
                                         [nsegs-ix-iy-1.0,         ix,  iy+1.0],
                                         [nsegs-ix-1.0-iy-1.0, ix+1.0,  iy+1.0]])
                     vertices = fracs.dot(cp)
-                    
+
                 #vert_list.append(vertices)
                 #color_list.append(color)
                 vert_list.append(eval_vert_to_lab(vertices))
                 color_list.append(eval_to_lab(theta,mag))
-            
+
     return vert_list, color_list
 
 
@@ -155,15 +155,15 @@ def UCS_cone_3D(ax,
 
     assert undisplayable_action in ['remove', 'clip', 'replace']
     #from matplotlib.patches import Polygon
-    
+
     def fix_vert_order(verts):
         order = [1,2,0]
         verts_out  = np.zeros((3,3))
         for i in range(3):
-            verts_out[i] = verts[i,order] 
+            verts_out[i] = verts[i,order]
         return verts_out
-    
-    
+
+
     vert_list, color_list = color_triangle_mesh(
                 my_map,
                 num_theta_levels = num_theta_levels,
@@ -182,7 +182,7 @@ def UCS_cone_3D(ax,
     color_list_out = []
     for vertices, color in zip(vert_list, color_list):
         sRGB1_color = cspace_convert(color, my_map.color_space, "sRGB1")
-        
+
         if use_deuteranomaly: sRGB1_color = cspace_convert(sRGB1_color, cvd_space, "sRGB1")
         if  np.any(sRGB1_color<0,0) or np.any(1.0<sRGB1_color):
             if undisplayable_action != 'remove':
@@ -192,24 +192,24 @@ def UCS_cone_3D(ax,
                     sRGB1_color = np.clip(sRGB1_color,0,1)
                 vert_list_out.append(fix_vert_order(vertices))
                 color_list_out.append(sRGB1_color)
-                
+
         else:
 #            ax.add_patch(  Polygon(vertices, color =np.clip(sRGB1_color,0,1), **triangle_style))
             vert_list_out.append(fix_vert_order(vertices))
             color_list_out.append(sRGB1_color)
 
-    poly = Poly3DCollection(vert_list_out, facecolors=color_list_out, alpha=alpha, **triangle_style)
+    poly = Poly3DCollection(vert_list_out, facecolors=color_list_out, edgecolors = color_list_out,  alpha=alpha, **triangle_style)
     ax.add_collection3d(poly, zdir='z')
     #art3d.Poly3DCollection(vert_list_out)
 
     #for vertices, sRGB1_color in zip(vert_list_out, color_list_out):
     #    ax.add_patch(  Polygon(vertices, color =sRGB1_color, **triangle_style))
-        
-    
+
+
     #if len(labels) ==3:
     #    cp = chemical_map.get_color_points()
     #    for i in range(3):
-    #        ax.text(cp[i,1], cp[i,2], cp[i,0]+ label_offset,labels[i] , ha = 'center', va = 'bottom', **font_options) 
+    #        ax.text(cp[i,1], cp[i,2], cp[i,0]+ label_offset,labels[i] , ha = 'center', va = 'bottom', **font_options)
         #ax.text(   0,             -label_offset, labels[0], ha = 'center', va = 'top',    **font_options)
         #ax.text(  width,         -label_offset, labels[1], ha = 'center', va = 'top',    **font_options)
         #ax.text( width/2.0, height+label_offset, labels[2], ha = 'center', va = 'bottom', **font_options)
@@ -217,17 +217,17 @@ def UCS_cone_3D(ax,
     ax.set_xlabel('a*')
     ax.set_ylabel('b*')
     ax.set_zlabel('L*')
-    
+
     theta_test = np.linspace(0,2*np.pi,my_map.min_theta_pts)
-    
+
     a_test = my_map.a_func_of_theta(theta_test)
     b_test = my_map.b_func_of_theta(theta_test)
-    
+
     ax.set_xlim(a_test.min(), a_test.max())
     ax.set_ylim(b_test.min(), b_test.max())
     ax.set_zlim(0, my_map.L_max)
 
-    
+
     #ax.set_aspect('equal','box')
     #ax.set_xlim(0,1)
     #ax.set_ylim(0,sin(60*pi/180))
